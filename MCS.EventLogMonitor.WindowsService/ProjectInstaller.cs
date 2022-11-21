c860748f828dd47f590df738d7ab952ca75a8ac9
DaveHodgeDev <47575373+DaveHodgeDev@users.noreply.github.com>
2022-11-21 14:17:09 -0500

    V1.0.8360 - Added the following features - Added Workspace ID & Key input options for the service installer. - Added auto-incrementation of version numbers for compiled files.

--- MCS.EventLogMonitor.WindowsService/ProjectInstaller.cs
+++ MCS.EventLogMonitor.WindowsService/ProjectInstaller.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+
 
 namespace MCS.EventLogMonitor.WindowsService
 {
@@ -19,6 +25,129 @@ namespace MCS.EventLogMonitor.WindowsService
         private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
         {
 
+        }
+
+        //public InstallerSetup()
+        //{
+        //    InitializeComponent();
+        //}
+
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            try
+            {
+                AddConfigurationFileDetails();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Failed to update the application configuration file: " + e.Message);
+                base.Rollback(savedState);
+            }
+        }
+
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+
+        private void showParameters()
+        {
+            StringBuilder sb = new StringBuilder();
+            StringDictionary myStringDictionary = this.Context.Parameters;
+
+            if (this.Context.Parameters.Count > 0)
+            {
+                foreach (string myString in this.Context.Parameters.Keys)
+                {
+                    sb.AppendFormat("String={0} Value= {1}\n", myString,
+                    this.Context.Parameters[myString]);
+                }
+            }
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void AddConfigurationFileDetails()
+        {
+            try
+            {
+                string WORKSPACEKEY = Context.Parameters["WORKSPACEKEY"].Trim();
+                string WORKSPACEID = Context.Parameters["WORKSPACEID"].Trim();
+                //MessageBox.Show("WorkspaceID: " + WORKSPACEID);
+                //MessageBox.Show("WorkspaceKey: " + WORKSPACEKEY);
+
+                // Get the path to the executable file that is being installed on the target computer  
+                string assemblypath = Context.Parameters["assemblypath"];
+                string appConfigPath = assemblypath + ".config";
+                //MessageBox.Show("appConfigPath: " + appConfigPath);
+
+                // Write the path to the app.config file  
+                XmlDocument doc = new XmlDocument();
+                doc.Load(appConfigPath);
+
+                XmlNode configuration = null;
+                foreach (XmlNode node in doc.ChildNodes)
+                    if (node.Name == "configuration")
+                        configuration = node;
+
+                if (configuration != null)
+                {
+                    //MessageBox.Show("configuration != null");  
+
+                    // Get the ‘appSettings’ node  
+                    XmlNode settingNode = null;
+                    foreach (XmlNode node in configuration.ChildNodes)
+                    {
+                        if (node.Name == "appSettings")
+                            settingNode = node;
+                    }
+
+                    if (settingNode != null)
+                    {
+                        //MessageBox.Show("settingNode != null");
+
+                        //Reassign values in the config file  
+                        foreach (XmlNode node in settingNode.ChildNodes)
+                        {
+                            //MessageBox.Show("node.Value = " + node.Value);  
+                            if (node.Attributes == null)
+                                continue;
+                            XmlAttribute attribute = node.Attributes["value"];
+                            //MessageBox.Show("attribute != null ");  
+
+                            if (node.Attributes["key"] != null)
+                            {
+                                switch (node.Attributes["key"].Value)
+                                {
+                                    case "workspaceId":
+                                        attribute.Value = WORKSPACEID;
+                                        break;
+
+                                    case "workspaceKey":
+                                        attribute.Value = WORKSPACEKEY;
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                    doc.Save(appConfigPath);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
